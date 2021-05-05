643ce8951690d40ead94fba7042a1adc172ca940
44615921+xdjeremy@users.noreply.github.com <jemoyfrancisco@gmail.com>
2021-05-05 12:12:11 +0800

    added balance count on summary

---
+++ inc/header.php
@@ -0,0 +1,24 @@
+<div id="content">
+	<nav class="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
+		<div class="container-fluid"><button class="btn btn-link d-md-none rounded-circle mr-3" id="sidebarToggleTop" type="button"><i class="fas fa-bars"></i></button>
+			<ul class="navbar-nav flex-nowrap ml-auto">
+				<li class="nav-item dropdown d-sm-none no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-toggle="dropdown" href="#"><i class="fas fa-search"></i></a>
+					<div class="dropdown-menu dropdown-menu-right p-3 animated--grow-in" aria-labelledby="searchDropdown">
+						<form class="form-inline mr-auto navbar-search w-100">
+							<div class="input-group"><input class="bg-light form-control border-0 small" type="text" placeholder="Search for ...">
+								<div class="input-group-append"><button class="btn btn-primary py-0" type="button"><i class="fas fa-search"></i></button></div>
+							</div>
+						</form>
+					</div>
+				</li>
+				<div class="d-none d-sm-block topbar-divider"></div>
+				<li class="nav-item dropdown no-arrow">
+					<div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-toggle="dropdown" href="#"><span class="d-none d-lg-inline mr-2 text-gray-600 small">Jeremy Francisco</span><img class="border rounded-circle img-profile" src="assets/img/avatars/avatar5.jpeg"></a>
+						<div class="dropdown-menu shadow dropdown-menu-right animated--grow-in"><a class="dropdown-item" href="#"><i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>&nbsp;Profile</a><a class="dropdown-item" href="#"><i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>&nbsp;Settings</a><a class="dropdown-item" href="#"><i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>&nbsp;Activity log</a>
+							<div class="dropdown-divider"></div><a class="dropdown-item" href="#"><i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>&nbsp;Logout</a>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</nav>
---
+++ inc/navbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0">
+	<div class="container-fluid d-flex flex-column p-0"><a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="#">
+			<div class="sidebar-brand-icon rotate-n-15"><i class="fas fa-coins"></i></div>
+			<div class="sidebar-brand-text mx-3"><span>Project Bank</span></div>
+		</a>
+		<hr class="sidebar-divider my-0">
+		<ul class="navbar-nav text-light" id="accordionSidebar">
+			<li class="nav-item"><a class="nav-link active" href="summary.php"><i class="fa fa-suitcase"></i><span>&nbsp;Account Summary</span></a></li>
+			<li class="nav-item"><a class="nav-link" href="transactions.html"><i class="icon ion-arrow-swap"></i><span>&nbsp;Payments &amp; Transfers</span></a></li>
+			<li class="nav-item"><a class="nav-link" href="send-money.html"><i class="icon ion-android-send"></i><span>&nbsp;Send Money</span></a></li>
+			<li class="nav-item"><a class="nav-link" href="transactions.html"><i class="fas fa-user-friends"></i><span>&nbsp;Accounts</span></a></li>
+			<li class="nav-item"><a class="nav-link" href="transactions.html"><i class="far fa-question-circle"></i><span>&nbsp;Waiting for Apporoval</span></a></li>
+		</ul>
+		<div class="text-center d-none d-md-inline"><button class="btn rounded-circle border-0" id="sidebarToggle" type="button"></button></div>
+	</div>
+</nav>
