e212068affb6fbff687b01725c18fd4fc603e561
44615921+xdjeremy@users.noreply.github.com <jemoyfrancisco@gmail.com>
2021-05-06 15:21:23 +0800

    added gitignore to database

--- inc/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$hostname = "localhost";
-$user = "root";
-$pass = "";
-$database = "project_bank";
-
-$dsn = 'mysql:host=' . $hostname . ';dbname=' . $database;
-$dbh = new PDO($dsn, $user, $pass);
